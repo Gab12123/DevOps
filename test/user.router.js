67528772ecf62bdbff637ae88da38ea204f47063
Gab12123 <gabrielle.korkmaz@edu.ece.fr>
2021-10-28 22:44:08 +0200

    Adding lab4

---
+++ test/user.router.js
@@ -0,0 +1,97 @@
+const app = require('../src/index')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+
+chai.use(chaiHttp)
+
+let client
+
+describe('User REST API', () => {
+
+  before(() => {
+    client = require('../src/dbClient')
+  })
+  
+  after(()=> {
+    app.close()
+    client.quit()
+  })
+
+  describe('POST /user', () => {
+
+    it('create a new user', (done) => {
+      const user = {
+        username: 'sergkudinov',
+        firstname: 'Sergei',
+        lastname: 'Kudinov'
+      }
+      chai.request(app)
+        .post('/user')
+        .send(user)
+        .then((res) => {
+          chai.expect(res).to.have.status(201)
+          chai.expect(res.body.status).to.equal('success')
+          chai.expect(res).to.be.json
+          done()
+        })
+        .catch((err) => {
+           throw err
+        })
+    })
+    
+    it('pass wrong parameters', (done) => {
+      const user = {
+        firstname: 'Sergei',
+        lastname: 'Kudinov'
+      }
+      chai.request(app)
+        .post('/user')
+        .send(user)
+        .then((res) => {
+          chai.expect(res).to.have.status(400)
+          chai.expect(res.body.status).to.equal('error')
+          chai.expect(res).to.be.json
+          done()
+        })
+        .catch((err) => {
+           throw err
+        })
+    })
+  })
+
+   describe('GET /user', ()=> {
+     // TODO Create test for the get method
+     it('Get successfully user', (done) => {
+      const user = {
+        username: 'sergkudinov',
+        firstname: 'Sergei',
+        lastname: 'Kudinov'
+      }
+      chai.request(app)
+        .get('/user/' + user.username)
+        .then((res) => {
+          chai.expect(res).to.have.status(200)
+          chai.expect(res.body.status).to.equal('success')
+          chai.expect(res).to.be.json
+          done()
+        })
+        .catch((err) => {
+           throw err
+        })
+     })
+
+     it('Cannot get a user when it does not exist', (done) => {
+      chai.request(app)
+      .get('/user/invalid')
+      .then((res) => {
+        chai.expect(res).to.have.status(400)
+        chai.expect(res.body.status).to.equal('error')
+        chai.expect(res).to.be.json
+        done()
+      })
+      .catch((err) => {
+         throw err
+      })
+    })
+   })
+})
